9d24ba5a554b3ae9d21604e5a9c2b7b53ac3f3df
Antonio Ruggiero <antonrugg@gmail.com>
2023-01-15 19:26:10 +0100

    feature: getting planet names from swapi to use as secret words. Added function to create keyboard programmatically. Added function which listen for key buttons pressed.

---
+++ keyboard.js
@@ -0,0 +1,132 @@
+const keyBoardButtons = [
+    {
+        letter: 'A',
+        id: 1,
+        disabled: false,
+    },
+     {
+        letter: 'B',
+        id: 2,
+        disabled: false,
+    },
+      {
+        letter: 'C',
+        id: 3,
+        disabled: false,
+    },
+       {
+        letter: 'D',
+        id: 4,
+        disabled: false,
+    },
+        {
+        letter: 'E',
+        id: 5,
+        disabled: false,
+    },
+         {
+        letter: 'F',
+        id: 6,
+        disabled: false,
+    },
+          {
+        letter: 'G',
+        id: 7,
+        disabled: false,
+    },
+           {
+        letter: 'H',
+        id: 8,
+        disabled: false,
+    },
+            {
+        letter: 'I',
+        id: 9,
+        disabled: false,
+    },
+             {
+        letter: 'J',
+        id: 10,
+        disabled: false,
+    },
+              {
+        letter: 'K',
+        id: 11,
+        disabled: false,
+    },
+               {
+        letter: 'L',
+        id: 12,
+        disabled: false,
+    },
+     {
+        letter: 'M',
+        id: 13,
+        disabled: false,
+    },
+     {
+        letter: 'N',
+        id: 14,
+        disabled: false,
+    },
+     {
+        letter: 'O',
+        id: 15,
+        disabled: false,
+    },
+     {
+        letter: 'P',
+        id: 16,
+        disabled: false,
+    },
+     {
+        letter: 'Q',
+        id: 17,
+        disabled: false,
+    },
+     {
+        letter: 'R',
+        id: 18,
+        disabled: false,
+    },
+     {
+        letter: 'S',
+        id: 19,
+        disabled: false,
+    },
+     {
+        letter: 'T',
+        id: 20,
+        disabled: false,
+    }, 
+      {
+        letter: 'U',
+        id: 21,
+        disabled: false,
+    },
+       {
+        letter: 'V',
+        id: 22,
+        disabled: false,
+    },
+        {
+        letter: 'W',
+        id: 23,
+        disabled: false,
+    },
+         {
+        letter: 'X',
+        id: 24,
+        disabled: false,
+    },
+          {
+        letter: 'Y',
+        id: 25,
+        disabled: false,
+    },
+           {
+        letter: 'Z',
+        id: 26,
+        disabled: false,
+    }
+]
--- solution.js
+++ solution.js
@@ -1,1 +1,63 @@
-console.log("ok");
+//console.log(keyBoardButtons);
+//tastiera a video realizzata sul layout
+//per le parole da indovinare proviamo ad usare una api swapi
+//quando ho finito la partita avro un button che resetta
+
+/*
+keyBoardButtons.map((keyBoardButton) => {
+  console.log(keyBoardButton.letter);
+});
+*/
+
+const keyBoardSection = document.getElementById("keyboard");
+
+function createKeyBoard(keyBoard) {
+  keyBoard.forEach((element) => {
+    const newButton = document.createElement("button");
+    newButton.classList.add("btn");
+    const buttonLetter = document.createTextNode(element.letter);
+    newButton.appendChild(buttonLetter);
+    keyBoardSection.appendChild(newButton);  
+  });
+    keyPressed();
+}
+
+function keyPressed() {
+    const buttons = document.querySelectorAll('.btn');
+    let letterPressed = '';
+    const lettersPressed = [];
+    buttons.forEach((button) => {
+        button.addEventListener('click', ()=> {
+            letterPressed = button.textContent;
+            lettersPressed.push(letterPressed);
+            button.disabled = true;
+        })
+    });
+}
+
+async function getSecretWords() {
+    const secretWords = [];
+    await fetch('https://swapi.dev/api/planets')
+        .then((response) => response.json())
+        .then((data) => {
+            data.results.map((planet) => {
+                secretWords.push(planet.name.toUpperCase());
+            })
+        });
+    console.log("secret words array: ", secretWords);
+    randomlyChosenWord(secretWords);
+}
+
+async function randomlyChosenWord(secrets) {
+    const randomNumberIndex = await Math.floor(Math.random() * secrets.length);
+    console.log("random secret word: ", secrets[randomNumberIndex]);
+    return secrets[randomNumberIndex];
+}
+ 
+
+
+
+
+createKeyBoard(keyBoardButtons);
+getSecretWords();
+
