0a873340ac4129718666329e98f7553cb3cff21d
Antonio Ruggiero <antonrugg@gmail.com>
2023-01-16 20:01:46 +0100

    feature: Implementata logica per visualizzare di volta in volta le lettere che indoviniamo.

--- solution.js
+++ solution.js
@@ -10,54 +10,95 @@ keyBoardButtons.map((keyBoardButton) => {
 */
 
 const keyBoardSection = document.getElementById("keyboard");
+const hangWordContainer = document.querySelector(".hang-word");
 
-function createKeyBoard(keyBoard) {
-  keyBoard.forEach((element) => {
+async function createKeyBoard(keyBoard) {
+  keyBoard.forEach((key) => {
     const newButton = document.createElement("button");
     newButton.classList.add("btn");
-    const buttonLetter = document.createTextNode(element.letter);
+    const buttonLetter = document.createTextNode(key.letter);
     newButton.appendChild(buttonLetter);
-    keyBoardSection.appendChild(newButton);  
+    keyBoardSection.appendChild(newButton);
   });
-    keyPressed();
+    const secretWords = await getSecretWords();
+    const letterPressed = keyPressed(secretWords);
+
 }
 
-function keyPressed() {
-    const buttons = document.querySelectorAll('.btn');
-    let letterPressed = '';
-    const lettersPressed = [];
-    buttons.forEach((button) => {
-        button.addEventListener('click', ()=> {
-            letterPressed = button.textContent;
-            lettersPressed.push(letterPressed);
-            button.disabled = true;
-        })
+function keyPressed(randomSecretWord) {
+  const buttons = document.querySelectorAll(".btn");
+  let letterPressed = "";
+  const lettersPressed = [];
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      letterPressed = button.textContent;
+      lettersPressed.push(letterPressed);
+        button.disabled = true;
+        console.log("rnd",randomSecretWord);
+        const isGuessed = checkIfGuessed(randomSecretWord, letterPressed);
+        if (isGuessed) {
+            const paragraphsLowDash = document.querySelectorAll('p');
+            console.log(paragraphsLowDash);
+            let revealWord = '';
+            paragraphsLowDash.forEach((paragraph, i) => {
+              //devo sostiuire il contenuto della p lowdash _ con la lettera indovinata se indovinata!
+              if (letterPressed === randomSecretWord[i] && paragraph.innerHTML !== letterPressed) {
+                paragraph.innerHTML = randomSecretWord[i]
+              }
+            })
+             
+        } else {
+            console.log("ehila");
+        }
     });
+  });
+    return letterPressed;
 }
 
 async function getSecretWords() {
-    const secretWords = [];
-    await fetch('https://swapi.dev/api/planets')
-        .then((response) => response.json())
-        .then((data) => {
-            data.results.map((planet) => {
-                secretWords.push(planet.name.toUpperCase());
-            })
-        });
+  const secretWords = [];
+  await fetch("https://swapi.dev/api/planets")
+    .then((response) => response.json())
+    .then((data) => {
+      data.results.map((planet) => {
+        secretWords.push(planet.name.toUpperCase());
+      });
+    });
     console.log("secret words array: ", secretWords);
-    randomlyChosenWord(secretWords);
+    const randomSecretWord = randomlyChosenWord(secretWords);
+    return randomSecretWord;
 }
 
-async function randomlyChosenWord(secrets) {
-    const randomNumberIndex = await Math.floor(Math.random() * secrets.length);
+function randomlyChosenWord(secrets) {
+  const randomNumberIndex = Math.floor(Math.random() * secrets.length);
     console.log("random secret word: ", secrets[randomNumberIndex]);
+    
+    createHangBoard(secrets[randomNumberIndex]);
     return secrets[randomNumberIndex];
 }
- 
+
+function createHangBoard(randomSecretWord) {
+    const splittedWord = randomSecretWord.split("");
+    console.log("splitted",splittedWord);
+    splittedWord.forEach((letter) => {
+        const newVoidLowDash = document.createElement("p");
+        newVoidLowDash.classList.add("low-dash");
+        const pLowDash = document.createTextNode("_");
+        newVoidLowDash.appendChild(pLowDash);
+        hangWordContainer.appendChild(newVoidLowDash);
+    });
+}
+
+function checkIfGuessed(secretWord, letterPressed) {
+    if (secretWord.includes(letterPressed)) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 
 
 
 createKeyBoard(keyBoardButtons);
-getSecretWords();
 
